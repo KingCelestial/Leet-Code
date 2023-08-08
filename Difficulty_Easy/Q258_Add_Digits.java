4067b2a0592d99310ed62d587de46fad2b1db900
King Celestial <sauravsharma2357@gmail.com>
2023-08-08 17:04:56 +0530

    Q-258

--- Difficulty_Easy/Q258_Add_Digits.java
+++ Difficulty_Easy/Q258_Add_Digits.java
@@ -5,23 +5,32 @@ import java.util.Scanner;
 public class Q258_Add_Digits {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner kc = new Scanner(System.in);
-		int n = kc.nextInt();
-		while(n!=0) {
-			int rem=0;
-			int sum=0;
-			while(n!=0) {
-				rem=n%10;
-				sum=sum+rem;
-				n=n/10;
+		int num = kc.nextInt();
+		System.out.println(addDigits(num));
+	}
+		public static int addDigits(int num) {
+			int count=counter(num);
+			while(count>1) {
+				int sum=0;
+				while(num>0) {
+					int rem = num%10;
+					sum=sum+rem;
+					num/=10;
+				}
+				num=sum;
+				count=counter(num);
 			}
-			n=sum;
-			if(n<=9) {
-				System.out.println(sum);
-				break;
-			}
+			return num;
+	}
+	public static int counter(int num) {
+		int count =0;
+		while(num>0) {
+			count++;
+			num/=10;
 		}
+		return count;
+		
 	}
 
 }

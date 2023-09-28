3c53ceab9aac72e6c7aca9f57d50652d7a63d1bc
King Celestial <139430697+KingCelestial@users.noreply.github.com>
2023-09-28 22:51:25 +0530

    Q-22

---
+++ Difficulty_Medium/Q22_Generate_Parentheses.java
@@ -0,0 +1,29 @@
+package Difficulty_Medium;
+
+//import java.util.ArrayList;
+import java.util.List;
+
+public class Q22_Generate_Parentheses {
+
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		int n =3;
+////		List<String> ll = new ArrayList<>();
+//		Generate_Paranthesis(n,0,0,"",ll);
+//		System.out.println(ll);
+//	}
+//	public static void Generate_Paranthesis(int n, int open, int close, String ans, List<String> ll) {
+//		if(open==n && close==n) {
+////			System.out.print(ans+" ");
+//			ll.add(ans);
+//			return;
+//		}
+//		if(open<n) {
+//			Generate_Paranthesis(n,open+1,close,ans+"(",ll);
+//		}
+//		if(close<open) {
+//			Generate_Paranthesis(n,open,close+1,ans+")",ll);
+//		}
+//	}
+
+}

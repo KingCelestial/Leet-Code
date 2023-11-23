1e720b2fe4589c0956a8503bfac89ccfe2eb9247
King Celestial <139430697+KingCelestial@users.noreply.github.com>
2023-11-23 07:22:12 +0530

    Q1630

---
+++ Difficulty_Medium/Q1630_Arithmetic_Subarrays.java
@@ -0,0 +1,19 @@
+package Difficulty_Medium;
+
+public class Q1630_Arithmetic_Subarrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] nums = {4,6,5,9,3,7};
+		int[] l = {0,0,2};
+		int[] r = {2,3,5};
+		check(nums,l,r);
+	}
+	public static void check(int[] nums, int[] l, int[] r) {
+//		recursion permutation code
+//		check for the subarray
+//		increse the values of l and r
+		//solved the array permutations in java question topic wise // recursion
+	}
+
+}
